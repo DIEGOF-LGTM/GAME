13586e163a7880afbdf3ff363c7b05b63922371f
DIEGOF-LGTM <jdiegoflorez@ucundinamarca.edu.co>
2025-04-28 11:50:08 -0500

    iniciando movimientos

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from personaje import personaje
+import costantes
+
 
 player = personaje(50,50)
 
 pygame.init()
-ancho = 800
-alto = 600
+ancho = costantes.ANCHO_VENTANA
+alto = costantes.ALTO_VENTANA
 
 ventana = pygame.display.set_mode((ancho, alto))
 pygame.display.set_caption("mi primer juego")
@@ -18,5 +20,17 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.k_a:
+                print("izquierda")
+            if event.key == pygame.k_d:
+                print("derecha ")
+            if event.key == pygame.k_w:
+                print("arriba")
+            if event.key == pygame.k_s:
+                print("abajo")
+                
+
+
     pygame.display.update()
 pygame.quit()
--- personaje.py
+++ personaje.py
@@ -1,7 +1,8 @@
 import pygame
+import costantes
 class personaje():
     def __init__(self, x,y):
-        self.forma = pygame.rect(0,0,20, 20)
+        self.forma = pygame.Rect(0,0,costantes.ANCHO_PERSONAJE,costantes.ALTO_PERSONAJE)
         self.forma.center = (x,y)
     def dibujar(self,ventana):
-        pygame.draw.rect (ventana, (255,255,0),self.forma)
+        pygame.draw.rect(ventana, costantes.COLOR_PERSONAJE,self.forma)
