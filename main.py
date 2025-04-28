1d548f5955c3859df5acb0aadbe352e4277bd454
DIEGOF-LGTM <jdiegoflorez@ucundinamarca.edu.co>
2025-04-28 11:16:49 -0500

    se agrego personaje a la ventana

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
+from personaje import personaje
+
+player = personaje(50,50)
 
 pygame.init()
 ancho = 800
 alto = 600
 
 ventana = pygame.display.set_mode((ancho, alto))
+pygame.display.set_caption("mi primer juego")
+
 
 run = True
 
 while run:
+    player.dibujar(ventana)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
 pygame.quit()
---
+++ personaje.py
@@ -0,0 +1,7 @@
+import pygame
+class personaje():
+    def __init__(self, x,y):
+        self.forma = pygame.rect(0,0,20, 20)
+        self.forma.center = (x,y)
+    def dibujar(self,ventana):
+        pygame.draw.rect (ventana, (255,255,0),self.forma)
